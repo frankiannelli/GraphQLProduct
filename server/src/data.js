4cc0a32283582c9b417e43a5de041df5ad366a00
frankiannelli <frank.iannelli@outlook.com>
2018-12-17 16:56:11 +1100

    initial commit

---
+++ server/src/data.js
@@ -0,0 +1,12 @@
+let invoice = {
+  id: 1, 
+  deliveryDate: '24-9-1983',
+  orderDate: '1-12-2018',
+  customerId:1, 
+  supplierId:1, 
+  lineitems: [
+    {productId: 1, qty: 1, price: 2}, 
+    {productId: 1, qty: 1, price: 2}
+  ]
+
+};
